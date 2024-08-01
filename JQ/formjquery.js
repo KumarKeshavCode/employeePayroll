bd1ea85f52690525a02ac6b1979b8086e427c352
KumarKeshavCode <kumarkeshav03030@gmail.com>
2024-08-01 16:48:15 +0530

    Add :Employee add in employeelist

---
+++ JQ/formjquery.js
@@ -0,0 +1,134 @@
+// $(document).ready(function(){
+//     $("#sbtn").click(function(){
+//         $(this).css("backgroundColor" , "red") ;
+//     })
+// })
+
+console.log("kky");
+
+$(document).ready(function(){
+    $("#sbtn").click(function(){
+ 
+        let employee={
+            name:$("#name").val(),
+            profile_img :$('input[name="profile_img"]:checked').val(),
+            gender :$('input[name="gender"]:checked').val(),
+            department:[],
+            //salary:$("#salary").val(),
+            salary :"₹ " +$("select[name='salary").val(),
+
+            
+            startday:$("#startdate").val()+" " +$("#month").val() + " "+$("#year").val(),
+            notes:$("#notes").val()
+        };
+        // console.log($('input[name="department"]:checked')) 
+        console.log("salary",""+$("select[name='salary").val())
+        $('input[name="department"]:checked').each(function(){
+            console.log($(this).val());  // get selected value
+
+            employee.department.push($(this).val());
+        });
+
+       
+
+        $.ajax({
+            url :'http://localhost:3000/employees',
+            type : 'POST',
+            data : JSON.stringify(employee),
+            contentType : 'application/json',
+            success : function(response){
+                console.log(response);
+               // appendEmployeeToTable(response);
+               // resetForm();
+                // window.location.href = '../index.html';
+            },
+            error:function(error){
+                console.log(error);
+            }
+        });
+    });
+
+
+
+    // edit Load data on form page
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const employeeId = urlParams.get('id');
+    
+
+    if(employeeId){
+        $.ajax({
+            url :`http://localhost:3000/employees/${employeeId}`,
+            type : 'GET',
+            success : function(employee){
+               $('#name').val(employee.name);
+               $(`input[name="profile_img"][valu="${employee.profile_img}"]`).prop('checked',true);
+               $(`input[name="gender"][value="${employee.gender}"]`).prop('checked',true);
+               $('#salary').val(employee.salary);
+               $('#startday').val(employee.startday);
+               $('#notes').val(employee.notes);
+
+               employee.department.forEach(function(dept){
+                $(`input[name="department"][value="${dept}"]`).prop('checked',true);
+               });
+            },
+            error:function(error){
+                console.log(error);
+            }
+        });
+    }
+
+
+    $("#sbtn").click(function(){
+        event.preventDefault();
+        let employee= {
+            id:employeeId,
+            name :$('#name').val(),
+            profile_img :$('input[name="profile_img"]:checked').val(),
+            gender :$('input[name="gender"]:checked').val(),
+            department:[],
+            salary:$('#salary').val(),
+            startday:$('#startday').val(),
+            notes:$('#notes').val()
+        };
+
+        $('input[name="department"]:checked').each(function(){
+            employee.department.push($(this).val());
+        });
+
+        let requestMethod = 'POST';
+        let requestUrl = 'https://localhost:3000/employees';
+
+        console.log("id is ",employeeId)
+        if(employeeId){
+
+            requestMethod = 'PUT';
+            requestUrl = `https://localhost:3000/employees/${employeeId}`;
+        }
+
+        // $.ajax({
+        //     url : requestUrl,
+        //     type : requestMethod,
+        //     data : JSON.stringify(employee),
+        //     contentType : 'application/json',
+        //     success : function(response){
+        //         console.log('response');
+        //         window.location.href = '../index.html';
+        //     },
+        //     error:function(error){
+        //         console.log(error);
+        //     }
+        // });
+    
+    });
+
+
+
+    $("#cancelbtn").click(function(){
+        window.location.href = '../index.html';
+    })
+
+
+});
+
+
--- jquery.js
+++ jquery.js
@@ -1,8 +1,3 @@
-$(function(){
-    $("#search").on("click", (function(){
-      $("#search").css("width", "200px");
-    }))
-  });
 
   $(function(){
 
@@ -11,3 +6,123 @@ $(function(){
     });
 
   });
+
+
+  
+
+  $(document).ready(function(){
+
+
+    
+  //   function appendEmployeeToTable(employee){
+  //     let row =   `<tr>
+                 
+                 
+  //                  <td>${employee.name}</td>
+  //                 <td>${employee.gender}</td>
+  //                 <td>${employee.department}</td>
+  //                 <td>${employee.salary}</td>
+  //                 <td>${employee.startday}</td>
+                  
+  //                 <td>
+  //                 <button class="editBtn" data-id="${employee.id}">Edit</button>
+  //                 <button class ="deleteBtn" data-id="${employee.id}">Delete</button>
+  //                 </tr>`;
+  //     $('#employeeTable tbody').append(row);
+  // }
+
+  // function resetForm(){
+  //     $("#employeeForm")[0].reset();
+  //     $('input[name="profile_img"]:checked').prop('checked',false);
+  //     $('input[name="gender"]:checked').prop('checked',false);
+  //     $('input[name="department"]:checked').prop('checked',false);
+
+  // }
+
+
+
+// $('#search').click(function(){
+//   $("#search").addClass('active').focus();
+// });
+
+// $('#search').blur(function(){
+//   $(this).removeClass('active');
+// });
+
+
+ $("#search").on("click", function() {
+        $(this).css("width", "200px");
+        $(this).addClass('active').focus();
+    }).on("blur", function() {
+        $(this).removeClass('active');
+    });
+
+
+  function loadEmployee(){
+      $.ajax({
+          url: 'http://localhost:3000/employees',
+          method: 'GET',
+          success: function(employees){
+              $('#employeeTable tbody').empty();
+              employees.forEach(function(employee) {
+              let row =   `<tr>
+                  <td>
+                  <img src=" ${employee.profile_img}">
+                  </td>
+                  <td>${employee.name}</td>
+                  <td>${employee.gender}</td>
+                  <td>${employee.department}</td>
+                  <td>${employee.salary}</td>
+                  <td>${employee.startday}</td>
+                  
+                  <td>
+                  <img  class="delbtn" data-id="${employee.id}"  src="Assets/delete-black-18dp.svg" alt="John Doe"  tittle="Delete"  style="width: 20px; height: 20px; margin-right: 5px; cursor:pointer;">
+         <img src="Assets/create-black-18dp.svg" class="editbtn" data-id="${employee.id}"alt="John Doe" style="width: 20px; height: 20px;  cursor:pointer; margin-right: 5px;">
+        
+                 
+                  </tr>`;
+              $('#employeeTable tbody').append(row);
+
+              });
+          },
+          error: function(error){
+              console.log('Error:' ,error);
+          }
+      });
+  }
+
+  loadEmployee();
+
+//employee table refresh function
+
+$("#bodytoptext").css("cursor", "pointer").click(function(){
+  loadEmployee();
+})
+
+  //Delete the row by delet icon
+
+  $(document).on('click','.delbtn',function(){
+    let id=$(this).data('id');
+
+    $.ajax({
+      url: `http://localhost:3000/employees/${id}`,
+      method: 'DELETE',
+      success: function(){
+          console.log('Employee deleted successfully');
+          loadEmployee();
+      },
+      error: function(error){
+          console.log('Error:' ,error);
+      }
+    })
+  })
+
+
+  $(document).on('click','.editbtn',function(){
+    let id=$(this).data('id');
+    window.location.href=`./template/userForm.html?id=${id}`;
+
+  });
+
+  })
+
